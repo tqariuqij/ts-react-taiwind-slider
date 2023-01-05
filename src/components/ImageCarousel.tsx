885fd479fed8ca5d97fbc9565d6dbf24db78bc4d
John Thiong'o <thiongojohn91@gmail.com>
2023-01-05 06:47:47 +0300

    first commit

---
+++ src/components/ImageCarousel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const ImageCarousel = () => {
+  return (
+    <div>
+      <h1>this is where the carousel will live</h1>
+    </div>
+  );
+};
+
+export default ImageCarousel;
